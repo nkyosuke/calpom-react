73b7d66be870f08070081121c6feb9b8a849d180
nkyosuke <puyol.5.gun.fk@gmail.com>
2024-12-05 01:43:36 +0900

    Calendarの表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import jaLocale from '@fullcalendar/core/locales/ja'; 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FullCalendar 
+        plugins={[dayGridPlugin]} 
+        initialView="dayGridMonth"
+        locales={[jaLocale]}
+        locale='ja'
+      />
     </div>
   );
 }
 
 export default App;
+
